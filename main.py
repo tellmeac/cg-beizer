2552b9da8581bae61901e0361f277c35bb0e3c43
Alexander Lipatov <tellmeac@gmail.com>
2022-10-16 19:54:46 +0700

    refactor: render herlpers at the end, fix ratio, disable buttons

--- main.py
+++ main.py
@@ -14,17 +14,20 @@
 
 import tkinter
 import math
+from tracemalloc import start
+from turtle import width
 
-LINE_PRECISION_STEP = 0.01
+# 0 < t < 1
+LINE_PRECISION_STEP = 0.001
 
 WIDTH = 1024
 HEIGHT = 768
 
-POINT_SIZE = 3
+POINT_SIZE = 4
 LINE_SIZE = 2
 
 SPLINE_COLOR = "gray"
-SPLINE_POINT_COLOR = "orange"
+SPLINE_POINT_COLOR = "green"
 BACKGROUND_COLOR = "white"
 
 
@@ -82,11 +85,11 @@ class BeizerSplines(tkinter.Frame):
         hide_btn = tkinter.Button(self, text="Hide helpers", command=self._hide_helpers)
         hide_btn.pack(side=tkinter.RIGHT, padx=5, pady=5)
 
-        reset_btn = tkinter.Button(self, text="Reset", command=self._reset)
-        reset_btn.pack(side=tkinter.RIGHT, padx=5, pady=5)
+        self._reset_btn = tkinter.Button(self, text="Reset", command=self._reset)
+        self._reset_btn.pack(side=tkinter.RIGHT, padx=5, pady=5)
 
-        start_btn = tkinter.Button(self, text="Start", command=self._start)
-        start_btn.pack(side=tkinter.RIGHT)
+        self._start_btn = tkinter.Button(self, text="Start", command=self._start)
+        self._start_btn.pack(side=tkinter.RIGHT)
 
         # declare event bindings
         left_click_event = "<Button-1>"
@@ -98,55 +101,54 @@ class BeizerSplines(tkinter.Frame):
         :param event: event
         """
         p = P(x=event.x, y=event.y)
-
         self._points.append(p)
-
         self._render_point(p, size=POINT_SIZE, color=SPLINE_POINT_COLOR)
 
     def _start(self):
-        start_point = self._points[0]
+        self._start_btn["state"] = tkinter.DISABLED
+        self._reset_btn["state"] = tkinter.ACTIVE
+
+        helpers_lines: list[(P, P)] = list()
+        herlpers_points: list[P] = list()
+
+        p4 = self._points[0] # used in two iterations
         for i in range(len(self._points) - 2):
             p1, p2, p3 = self._points[i], self._points[i+1], self._points[i+2]
 
-            aj1 = (p1 + p2) / 2
-            aj2 = (p2 + p3) / 2
+            # Relation between two vectors (p1, p2) and (p2, p3)
+            rel = math.hypot(p2.x - p1.x, p2.y - p1.y) / math.hypot(p3.x - p2.x, p3.y - p2.y)
 
-            lam = self._ratio_of_segments(p1, p2, p3)
+            a1 = (p1 + p2) / 2
+            a2 = (p2 + p3) / 2
 
-            bj = (aj1 + lam * aj2) / (1 + lam);
+            bj = (a1 + rel * a2) / (1 + rel);
 
-            pi1 = aj1 - bj + p2
-            pi2 = aj2 - bj + p2
+            h1 = a1 - bj + p2
+            h2 = a2 - bj + p2
 
-            self._render_beizer_line(p1, start_point, pi1, p2)
+            self._render_beizer_line(p1, p4, h1, p2)
+            helpers_lines.append((h1, h2))
+            herlpers_points.extend([p1, p4, h1, p2])
 
-            start_point = pi2
+            p4 = h2
 
-            # helpers
-            self._render_point(aj1, size=3, color="blue", tag="helpers")
-            self._render_point(aj2, size=3, color="blue", tag="helpers")
-            self._render_point(bj, size=3, color="green", tag="helpers")
-            self._render_point(pi1, size=3, color="purple", tag="helpers")
-            self._render_point(pi2, size=3, color="purple", tag="helpers")
+        # Render last, use last but one point twice.
+        self._render_beizer_line(self._points[-2], p4, self._points[-1], self._points[-1])
+        herlpers_points.extend([self._points[-2], p4, self._points[-1], self._points[-1]])
 
-            self._render_line(aj1, aj2, width=LINE_SIZE, color="green", tag="helpers")
-            self._render_line(pi1, pi2, width=LINE_SIZE, color="purple", tag="helpers")
-            # straight between two points
-            self._render_line(p1, p2, width=LINE_SIZE, color="blue", tag="helpers")
+        # Render helpers at the end.
+        for a, b in helpers_lines:
+            self._render_line(a, b, width=LINE_SIZE, color="orange", tag="helpers")
+        for a in herlpers_points:
+            print(f"participant: {a}")
+            self._render_point(a, size=POINT_SIZE, color="red", tag="helpers")
 
-        # render last two
-        self._render_beizer_line(self._points[-2], start_point, self._points[-1], self._points[-1])
-        self._render_line(self._points[-2], self._points[-1], width=LINE_SIZE, color="blue", tag="helpers")
-
-        # prevent starting from the whole start
-        self._points = self._points[-1:]
-
-    def _render_beizer_line(self, p1: P, p2: P, p3: P, p4: P):
+    def _render_beizer_line(self, p0: P, p1: P, p2: P, p3: P):
         t = 0.001
         while t <= 1:
-            new_point = self._beizer_next_point(p1, p2, p3, p4, t)
+            current_point = self._beizer_point(p0, p1, p2, p3, t)
 
-            self._render_point(new_point, size=LINE_SIZE)
+            self._render_point(current_point, size=LINE_SIZE - 2)
 
             t += LINE_PRECISION_STEP
 
@@ -163,8 +165,10 @@ class BeizerSplines(tkinter.Frame):
         self._cnv.create_line(a.x, a.y, b.x, b.y, fill=color, width=width, tags=tag)
 
     def _reset(self):
+        self._start_btn["state"] = tkinter.ACTIVE
+        self._reset_btn["state"] = tkinter.DISABLED
         self._cnv.delete("all")
-        self._points = []
+        self._points = list()
 
     def _hide_helpers(self):
         self._cnv.itemconfig("helpers", state="hidden")
@@ -173,12 +177,11 @@ class BeizerSplines(tkinter.Frame):
         self._cnv.itemconfig("helpers", state="normal")
 
     @staticmethod
-    def _ratio_of_segments(p1: P, p2: P, p3: P) -> float:
-        return math.hypot(p2.x - p1.x, p2.x - p1.x) / math.hypot(p3.x - p2.x, p3.x - p2.x)
-
-    @staticmethod
-    def _beizer_next_point(c1: P, c2: P, c3: P, c4: P, t: float) -> P:
-        return t ** 3 * (c4 - 3 * c3 + 3 * c2 - c1) + t ** 2 * (3 * c1 - 6 * c2 + 3 * c3) + t * (3 * c2 - 3 * c1) + c1
+    def _beizer_point(p1: P, p2: P, p3: P, p4: P, t: float) -> P:
+        """
+        Returns cubic beizer point by 4 base points and t in [0, 1] 
+        """
+        return t ** 3 * (p4 - 3 * p3 + 3 * p2 - p1) + t ** 2 * (3 * p1 - 6 * p2 + 3 * p3) + t * (3 * p2 - 3 * p1) + p1
 
 if __name__ == '__main__':
     base = tkinter.Tk()
